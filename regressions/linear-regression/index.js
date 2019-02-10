92d0182ed2b274672290ee8a003efd65f2f94c66
Brandon Ransom <brandon.ransom@libertymutual.com>
2019-02-10 18:28:10 -0500

    Implement logistic regression

---
+++ regressions/linear-regression/index.js
@@ -0,0 +1,42 @@
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("../load-csv");
+const LinearRegression = require("./linear-regression");
+const plot = require("node-remote-plot");
+
+let {
+  features,
+  labels,
+  testFeatures,
+  testLabels
+} = loadCSV("./cars.csv", {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ["horsepower", "weight", "displacement"],
+  labelColumns: ["mpg"]
+});
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.1,
+  iterations: 3,
+  batchSize: 10
+});
+
+regression.features.print();
+
+regression.train();
+const r2 = regression.test(testFeatures, testLabels);
+
+regression.weights.print();
+
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: "Value of B",
+  yLabel: "Mean Squared Error"
+});
+
+console.log("R2 is", r2);
+
+regression.predict([
+  [120, 2, 380]
+]).print();
--- regressions/linear-regression/linear-regression.js
+++ regressions/linear-regression/linear-regression.js
@@ -6,31 +6,57 @@ class LinearRegression {
   constructor(features, labels, options) {
     this.features = this.processFeatures(features);
     this.labels = tf.tensor(labels);
+    this.mseHistory = [];
 
     this.options = Object.assign({
       learningRate: 0.1,
-      iterations: 1000
+      iterations: 100,
+      batchSize: 1 // by default -> stochastic gradient descent
     }, options);
 
     this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
-  gradientDescent() {
-    const currentGuesses = this.features.matMul(this.weights);
-    const differences = currentGuesses.sub(this.labels);
+  gradientDescent(features, labels) {
+    const currentGuesses = features.matMul(this.weights);
+    const differences = currentGuesses.sub(labels);
 
-    const slopes = this.features
+    const slopes = features
       .transpose()
       .matMul(differences)
-      .div(this.features.shape[0])
+      .div(features.shape[0])
 
     this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
+    const batchQuantity = Math.floor(this.features.shape[0] / this.options.batchSize);
+
     for (let i = 0; i < this.options.iterations; i++) {
-      this.gradientDescent();
+      for (let j = 0; j < batchQuantity; j++) {
+        const {
+          batchSize
+        } = this.options;
+        const startIndex = j * batchSize;
+
+        const featureSlice = this.features.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        )
+        const labelSlice = this.labels.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        )
+
+        this.gradientDescent(featureSlice, labelSlice);
+      }
+      this.recordMSE();
+      this.updateLearningRate();
     }
+  }
+
+  predict(observations) {
+    return this.processFeatures(observations).matMul(this.weights);
   }
 
   test(testFeatures, testLabels) {
@@ -78,6 +104,32 @@ class LinearRegression {
     this.variance = variance;
 
     return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .get();
+
+    // unshift puts the value at the beginning of the array
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return;
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
+
   }
 }
 
